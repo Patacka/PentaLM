4a8c88a42fd230bdceb151cafbb676023adc003a
Patacka <pstrack@hotmail.de>
2024-02-21 23:18:13 +0000

    Update

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ class PPOUpdater(BaseUpdater):
         self.lr = config.lr
         self.clip_eps = config.clip_eps
         self.value_loss_coef = config.value_loss_coef
+        self.entropy_coef = config.entropy_coef
         self.max_grad_norm = config.max_grad_norm
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -71,8 +72,12 @@ class PPOUpdater(BaseUpdater):
             self.optimizer = torch.optim.SGD(self._iterator_trainable_params, lr=self.lr)
 
         current_process_buffer = {}
-        for k in ['advantages', 'returns', 'logprobs', 'values']:
-            current_process_buffer[k] = kwargs[k][_current_batch_ids].to(self._llm_module.device)
+        print(_current_batch_ids)
+        print("loop beginnt")
+        for x in _current_batch_ids:
+            for k in ['actions', 'advantages', 'returns', 'logprobs', 'values']:
+                current_process_buffer[k] = kwargs[k][x]
+
 
         epoch_losses = {
             "value": [],
@@ -91,14 +96,17 @@ class PPOUpdater(BaseUpdater):
             output = self._llm_module(['score', 'value'], contexts=_contexts, candidates=_candidates,
                                       require_grad=True, minibatch_size=self.minibatch_size)
             scores = torch.stack([_o['score'] for _o in output]).squeeze()
+            probas = torch.distributions.Categorical(logits=scores)
             values = torch.stack([_o["value"][0] for _o in output]).squeeze()
             scores = scores.to(self.device)
             # Compute policy loss
+            entropy = probas.entropy().mean()
+            log_prob = probas.log_prob(current_process_buffer['actions'][_start_idx:_stop_idx])
             #print("Scores")
             #print(scores)
             #print("logprobs: ")
             #print(current_process_buffer['logprobs'][_start_idx:_stop_idx])
-            ratio = torch.exp(scores - current_process_buffer['logprobs'][_start_idx:_stop_idx])
+            ratio = torch.exp(log_prob - current_process_buffer['logprobs'][_start_idx:_stop_idx])
             #print("ratio")
             #print(ratio)
             assert not (step == 0 and (torch.any(ratio < 0.95) or torch.any(ratio > 1.15))), "PPO ratio != 1 !!"
@@ -117,7 +125,7 @@ class PPOUpdater(BaseUpdater):
             epoch_losses["value"].append(value_loss.detach().cpu().item())
 
             # Compute final loss
-            loss = policy_loss + self.value_loss_coef * value_loss
+            loss = policy_loss - entropy * self.entropy_coef  + self.value_loss_coef * value_loss
             epoch_losses["loss"].append(loss.detach().cpu().item())
 
             # Backward
@@ -151,6 +159,7 @@ class PPOBuffer:
     def __init__(self, size, gamma=0.99, lambda_gae=0.95):
         self.obs_buf = [None for _ in range(size)]
         self.act_buf = [None for _ in range(size)]
+        self.possible_act_buf = [None for _ in range(size)]
         self.adv_buf = np.zeros(size, dtype=np.float32)
         self.rew_buf = np.zeros(size, dtype=np.float32)
         self.ret_buf = np.zeros(size, dtype=np.float32)
@@ -159,10 +168,11 @@ class PPOBuffer:
         self.gamma, self.lambda_gae = gamma, lambda_gae
         self.ptr, self.path_start_idx, self.max_size = 0, 0, size
 
-    def store(self, obs, act, rew, val, logp):
+    def store(self, obs, act, possible_act, rew, val, logp):
         assert self.ptr < self.max_size
         self.obs_buf[self.ptr] = obs
         self.act_buf[self.ptr] = act
+        self.possible_act_buf[self.ptr] = possible_act
         self.rew_buf[self.ptr] = rew
         self.val_buf[self.ptr] = val
         self.logp_buf[self.ptr] = logp
@@ -188,7 +198,7 @@ class PPOBuffer:
         # the next two lines implement the advantage normalization trick
         adv_mean, adv_std = np.mean(self.adv_buf), np.std(self.adv_buf)
         self.adv_buf = (self.adv_buf - adv_mean) / (adv_std + 1e-8)
-        data = dict(obs=self.obs_buf, act=self.act_buf, ret=self.ret_buf,
+        data = dict(obs=self.obs_buf, act=self.act_buf, possible_act=self.possible_act_buf, ret=self.ret_buf,
                     adv=self.adv_buf, logp=self.logp_buf, val=self.val_buf)
         return {
             k: torch.as_tensor(v, dtype=torch.float32)
@@ -256,7 +266,13 @@ def main(config: DictConfig):
         "the IP, without any additional characters, explanation."
         "Your Goal is to find and open the flag.txt file by exploit a vulnerability on the target system."
     )
+
     IP_under_test = "10.10.166.243"
+
+    choose_prompt_template= (
+        "Based on this observation: '{0}' and on these possible actions '{1}' choose one that help you to expoloit a vulnerability on "
+        "the target system and  find and open the flag.txt"
+    )
 
     episode_return = 0
     episode_length = 0
@@ -266,20 +282,25 @@ def main(config: DictConfig):
     for epoch in range(config.rl_script_args.epochs):
         for t in tqdm(range(config.rl_script_args.steps_per_epoch), ascii=" " * 9 + ">", ncols=100):
             task_prompt = task_prompt_template.format(observation, IP_under_test)
+
             rl_script_logger.info("Generating action...")
-            action = agent.generate([task_prompt], **config.lamorel_args.llm_args.generation_args)[ZERO_AS_BS_IS_1][0]['text']
+            action_space = []
+            for x in range(3):
+                action_space.append(agent.generate([task_prompt], **config.lamorel_args.llm_args.generation_args)[ZERO_AS_BS_IS_1][0]['text'])
+            choose_prompt = choose_prompt_template.format(action_space, observation)
+            action = agent.generate([choose_prompt], **config.lamorel_args.llm_args.generation_args)[ZERO_AS_BS_IS_1][0]['text']
             rl_script_logger.info(f"Generated action: {action}")
             rl_script_logger.info(f"Epoch Step: {t}")
-            action = action.replace(task_prompt, '')
+            action = action.replace(choose_prompt, '')
             lines = action.split('\n')
             action = '\n'.join(lines[1:]) if len(lines) > 1 else ""
             action = action.replace("<|im_end|>", '')
 
-            score_and_value = agent.custom_module_fns(['score', 'value'], contexts=[task_prompt], candidates=[[action]])[ZERO_AS_BS_IS_1]
+            score_and_value = agent.custom_module_fns(['score', 'value'], contexts=[choose_prompt], candidates=[action])[ZERO_AS_BS_IS_1]
             observation, reward, done, _ = env.step(action)
 
             epoch_ended = t+1 == config.rl_script_args.steps_per_epoch
-            buffer.store(task_prompt, [action], reward, score_and_value['value'][0], score_and_value['score'][0])
+            buffer.store(task_prompt, [action], [action_space], reward, score_and_value['value'][0], score_and_value['score'][0])
             episode_return += reward
             episode_length += 1
             timeout = episode_length == config.rl_script_args.max_episode_length
@@ -299,12 +320,13 @@ def main(config: DictConfig):
         rl_script_logger.info(f"PPO update number {epoch + 1}")
         trajectories = buffer.get()
         update_results = agent.update(trajectories['obs'],
-                                            trajectories['act'],
-                                            returns=trajectories['ret'],
-                                            advantages=trajectories['adv'],
-                                            logprobs=trajectories['logp'],
-                                            values=trajectories['val'],
-            )[ZERO_AS_BS_IS_1]
+                                        trajectories['possible_act'],
+                                        actions=trajectories['act'],
+                                        returns=trajectories['ret'],
+                                        advantages=trajectories['adv'],
+                                        logprobs=trajectories['logp'],
+                                        values=trajectories['val'],
+                                        )[ZERO_AS_BS_IS_1]
         history["loss"].append(update_results['loss'])
         history["policy_loss"].append(update_results['policy_loss'])
         history["value_loss"].append(update_results['value_loss'])
